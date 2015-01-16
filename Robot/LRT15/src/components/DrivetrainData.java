fe5dc562b2d606915c5cb5053ecfe9764b1ed4d3
Shadaj Laddad <shadaj@users.noreply.github.com>
2015-01-16 03:56:08 +0000

    Initial implementation of DrivetrainData
    + Mostly a conversion of last year's C++ to Java
    
    git-svn-id: http://svn.lynbrookrobotics.com/code15@25 9f8849d2-cf13-4b18-a094-84ffabbe6057

---
+++ Robot/LRT15/src/components/DrivetrainData.java
@@ -0,0 +1,68 @@
+public class DrivetrainData extends ComponentData {
+  public static final int VELOCITY_X = 0;
+  public static final int VELOCITY_Y = 1;
+  public static final int VELOCITY_ROTATION = 2;
+
+  private double[] desiredRates = new double[3];
+  private double[] desiredOpenLoopOutputs = new double[3];
+  private boolean[] resetPositionSetpoint = new boolean[3];
+  private double[] positionSetpoints = new double[3];
+  private double[] maxSpeeds = new double[3];
+
+  public DrivetrainData(String name) {
+    super(name);
+    ResetCommands();
+  }
+
+  public double getVelocity(int type) {
+    return desiredRates[type];
+  }
+
+  public void setVelocity(int type, double newValue) {
+    desiredRates[type] = newValue;
+  }
+
+  public double getOpenLoopOutput(int type) {
+    return desiredOpenLoopOutputs[type];
+  }
+
+  public void setOpenLoopOutput(int type, double newValue) {
+    desiredOpenLoopOutputs[type] = newValue;
+  }
+
+  public double getPositionSetpoint(int type) {
+    return positionSetpoints[type];
+  }
+
+  public void setPositionSetpoint(int type, double newValue) {
+    resetPositionSetpoint[type] = false;
+    positionSetpoints[type] = newValue;
+  }
+
+  public void setRelativePositionSetpoint(int type, double newValue) {
+    resetPositionSetpoint[type] = false;
+    positionSetpoints[type] += newValue;
+  }
+
+  public double getPositionControlMaxSpeed(int type) {
+    return maxSpeeds[type];
+  }
+
+  public void setPositionControlMaxSpeed(int type, double newValue) {
+    maxSpeeds[type] = newValue;
+  }
+
+  @Override
+  protected void ResetCommands() {
+    int[] types = { VELOCITY_X, VELOCITY_Y, VELOCITY_ROTATION };
+    for (int type: types) {
+      desiredRates[type] = 0;
+      desiredOpenLoopOutputs[type] = 0;
+      resetPositionSetpoint[type] = true;
+      positionSetpoints[type] = 0;
+    }
+    setVelocity(VELOCITY_X, 0.0);
+    setVelocity(VELOCITY_Y, 0.0);
+    setVelocity(VELOCITY_ROTATION, 0.0);
+  }
+}
