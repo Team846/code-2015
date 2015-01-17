8944b8bc90d5d95997ce5856e174eab07c72927d
RiaPradeep <RiaPradeep@9f8849d2-cf13-4b18-a094-84ffabbe6057>
2015-01-17 03:24:42 +0000

    Updated ElevatorData with two new functions. -Ria Pradeep
    git-svn-id: http://svn.lynbrookrobotics.com/code15@31 9f8849d2-cf13-4b18-a094-84ffabbe6057

--- Robot/LRT15/src/components/ElevatorData.java
+++ Robot/LRT15/src/components/ElevatorData.java
@@ -3,27 +3,33 @@ package components;
 public class ElevatorData extends ComponentData{
 	
 	public static int SPEED = 5;
-
 	
 	public static final int STAY = 1;
-	public static final int UP= 2;
+	public static final int UP = 2;
 	public static final int DOWN= 3;
+	private static int motion = 1;
+	
+	public static final int GROUND = 0;
+	public static final int TOTE1 = 5;
+	public static final int TOTE2 = 10;
+	public static final int TOTE3 = 15;
 	private static int state = 1;
+	
 
 	public ElevatorData(String name) {
 		super(name);
 		// TODO Auto-generated constructor stub
 	}
 	
-	public int getState(){
-		return state; 
+	public int getCurrentMotion(){
+		return motion; 
 	}
 	
-	public int setDesiredState(int i){
+	public int setDesiredMotion(int i){
 		if(i == STAY || i == UP || i == DOWN){
-			state = i;
+			motion = i;
 		}
-		return state; 
+		return motion; 
 	}
 	
 	public int getMaxSpeed(){
@@ -33,6 +39,17 @@ public class ElevatorData extends ComponentData{
 	public int setMaxSpeed(int desiredSpeed){
 		SPEED = desiredSpeed;
 		return SPEED;
+	}
+	
+	public int getState(){
+		return state;
+	}
+	
+	public int setDesiredState(int i){
+		if(i == GROUND || i == TOTE1 || i == TOTE2 || i == TOTE3){
+			state = i;
+		}
+		return state;
 	}
 	
 	
