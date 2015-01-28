277f81c9711a7aedb91b24adb876f8011686da64
RiaPradeep <RiaPradeep@9f8849d2-cf13-4b18-a094-84ffabbe6057>
2015-01-28 05:28:23 +0000

    Updated Elevator.java
    git-svn-id: http://svn.lynbrookrobotics.com/code15@59 9f8849d2-cf13-4b18-a094-84ffabbe6057

--- Robot/LRT15/src/components/Elevator.java
+++ Robot/LRT15/src/components/Elevator.java
@@ -17,7 +17,6 @@ public class Elevator extends Component {
 	//LRTSpeedController motor;
 
 	public static double speed;
-	
 	public static int setpoint;
 	public static double distance;
 	
@@ -30,83 +29,47 @@ public class Elevator extends Component {
 		motor = new LRTTalon(CHANGEME, changeString, CHANGEME1);
 		ultrasonic = new Ultrasonic(null, null);
 		elevatorData = (ElevatorData) ComponentData.GetComponentData("ElevatorData");
-		// TODO Auto-generated constructor stub
 	}
 
-	public double distanceToTop(){
-		return ultrasonic.getRangeMM();
-	}
-	
-	public void toTheTop(){
-		speed = elevatorData.toTheTop();
-		motor.SetDutyCycle(speed);
-	}
+
 
 	@Override
 	protected void UpdateEnabled() {
-		// TODO Auto-generated method stub
-		/*
-		 * Receiving commands
-		 */
 
-		setpoint = elevatorData.getState();
-		if(elevatorData.numberOfTotes==1){
+		setpoint = elevatorData.getDesiredState();
+		//assuming we know the number of totes the robot is carrying
+		if(elevatorData.numberOfTotes==1) {
 			distance = ultrasonic.getRangeInches() + elevatorData.TOTE1 - setpoint;
-			speed = (distance * elevatorData.getMaxSpeed())/50;
+			speed = (distance * elevatorData.getMaxSpeed())/50; //velocity?
 			motor.SetDutyCycle(speed);
 		}
 		
-		else if(elevatorData.numberOfTotes==2){
+		else if(elevatorData.numberOfTotes==2) {
 			distance = ultrasonic.getRangeInches() + elevatorData.TOTE2 - setpoint;
 			speed = (distance * elevatorData.getMaxSpeed())/50;
 			motor.SetDutyCycle(speed);
 		}
 		
-		else if(elevatorData.numberOfTotes==3){
+		else if(elevatorData.numberOfTotes==3) {
 			distance = ultrasonic.getRangeInches() + elevatorData.TOTE3 - setpoint;
 			speed = (distance * elevatorData.getMaxSpeed())/50;
 			motor.SetDutyCycle(speed);
 		}
-		
-		/*distance = ultrasonic.getRangeInches() - setpoint;
-		speed = (distance * elevatorData.getMaxSpeed())/50;
-		motor.SetDutyCycle(speed);*/
 	
-		
-		
-		/*if () {
-			elevatorData.distance = ultrasonic.getRangeInches() - elevatorData.getState();
-			speed = (elevatorData.distance * elevatorData.getMaxSpeed())/50;
-			motor.SetDutyCycle(speed);
-		}
-		else if (downButton) {
-			elevatorData.distance = ultrasonic.getRangeInches() - elevatorData.getState();
-			speed = (elevatorData.distance * elevatorData.getMaxSpeed())/50;
-			motor.SetDutyCycle(speed);
-		}
-		else if (!upButton && !downButton) {
-			motor.SetDutyCycle(0.0);
-			elevatorData.getState();
-			elevatorData.getMaxSpeed();
-			elevatorData.getCurrentMotion();
-		}*/
 	}
 
 	@Override
 	protected void UpdateDisabled() {
-		// TODO Auto-generated method stub	
 		motor.SetDutyCycle(0.0);
 	}
 
 	@Override
 	protected void OnEnabled() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	protected void OnDisabled() {
-		// TODO Auto-generated method stub
 		/*
 		 * Reset robot elevator to normal height
 		 */
