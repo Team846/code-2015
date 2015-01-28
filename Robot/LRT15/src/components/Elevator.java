8efd7490af09b279966c71af63f720654ab22ec8
RamachandranDamodaran <RamachandranDamodaran@9f8849d2-cf13-4b18-a094-84ffabbe6057>
2015-01-28 04:58:00 +0000

    Updated Elevator.java *Not complete* - Ram D., Ria P.
    git-svn-id: http://svn.lynbrookrobotics.com/code15@57 9f8849d2-cf13-4b18-a094-84ffabbe6057

--- Robot/LRT15/src/components/Elevator.java
+++ Robot/LRT15/src/components/Elevator.java
@@ -3,25 +3,45 @@ import actuators.LRTSpeedController;
 import actuators.LRTTalon;
 import componentData.ComponentData;
 import componentData.ElevatorData;
+import edu.wpi.first.wpilibj.Ultrasonic;
 
 public class Elevator extends Component {
-	
+
 	private ElevatorData elevatorData;
-	
+
 	private boolean upButton;
 	private boolean downButton;
 	int CHANGEME;
 	String changeString;
 	int CHANGEME1;
-	LRTSpeedController motor;
+	//LRTSpeedController motor;
+
+	public static double speed;
+	
+	public static int setpoint;
+	public static double distance;
+	
+
+	public Ultrasonic ultrasonic;
+	public LRTTalon motor;
 
 	public Elevator(String name, int driverStationDigitalIn) {
 		super(name, driverStationDigitalIn);
-		LRTTalon motor = new LRTTalon(CHANGEME, changeString, CHANGEME1);
+		motor = new LRTTalon(CHANGEME, changeString, CHANGEME1);
+		ultrasonic = new Ultrasonic(null, null);
 		elevatorData = (ElevatorData) ComponentData.GetComponentData("ElevatorData");
 		// TODO Auto-generated constructor stub
 	}
+
+	public double distanceToTop(){
+		return ultrasonic.getRangeMM();
+	}
 	
+	public void toTheTop(){
+		speed = elevatorData.toTheTop();
+		motor.SetDutyCycle(speed);
+	}
+
 	@Override
 	protected void UpdateEnabled() {
 		// TODO Auto-generated method stub
@@ -29,40 +49,59 @@ public class Elevator extends Component {
 		 * Receiving commands
 		 */
 
-		if (upButton) {
-			//move elevator up at desired speed		
-			elevatorData.getState();
-			elevatorData.getMaxSpeed();
-			elevatorData.getCurrentMotion();
-			//set motor according to data
-		}
-		else if (downButton) {
-			//move elevator down at desired speed
-			elevatorData.getState();
-			elevatorData.getMaxSpeed();
-			elevatorData.getCurrentMotion();
-			//set motor according to data
-		}
-		else if (!upButton && !downButton) {
-			//stops elevator from moving
-			elevatorData.getState();
-			elevatorData.getMaxSpeed();
-			elevatorData.getCurrentMotion();
-			//set motor according to data
+		setpoint = elevatorData.getState();
+		if(elevatorData.numberOfTotes==1){
+			distance = ultrasonic.getRangeInches() + elevatorData.TOTE1 - setpoint;
+			speed = (distance * elevatorData.getMaxSpeed())/50;
+			motor.SetDutyCycle(speed);
 		}
 		
+		else if(elevatorData.numberOfTotes==2){
+			distance = ultrasonic.getRangeInches() + elevatorData.TOTE2 - setpoint;
+			speed = (distance * elevatorData.getMaxSpeed())/50;
+			motor.SetDutyCycle(speed);
+		}
+		
+		else if(elevatorData.numberOfTotes==3){
+			distance = ultrasonic.getRangeInches() + elevatorData.TOTE3 - setpoint;
+			speed = (distance * elevatorData.getMaxSpeed())/50;
+			motor.SetDutyCycle(speed);
+		}
+		
+		/*distance = ultrasonic.getRangeInches() - setpoint;
+		speed = (distance * elevatorData.getMaxSpeed())/50;
+		motor.SetDutyCycle(speed);*/
+	
+		
+		
+		/*if () {
+			elevatorData.distance = ultrasonic.getRangeInches() - elevatorData.getState();
+			speed = (elevatorData.distance * elevatorData.getMaxSpeed())/50;
+			motor.SetDutyCycle(speed);
+		}
+		else if (downButton) {
+			elevatorData.distance = ultrasonic.getRangeInches() - elevatorData.getState();
+			speed = (elevatorData.distance * elevatorData.getMaxSpeed())/50;
+			motor.SetDutyCycle(speed);
+		}
+		else if (!upButton && !downButton) {
+			motor.SetDutyCycle(0.0);
+			elevatorData.getState();
+			elevatorData.getMaxSpeed();
+			elevatorData.getCurrentMotion();
+		}*/
 	}
 
 	@Override
 	protected void UpdateDisabled() {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub	
+		motor.SetDutyCycle(0.0);
 	}
 
 	@Override
 	protected void OnEnabled() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -72,5 +111,6 @@ public class Elevator extends Component {
 		 * Reset robot elevator to normal height
 		 */
 	}
+
 
 }
