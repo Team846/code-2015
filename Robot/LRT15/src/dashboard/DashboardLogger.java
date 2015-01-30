072693b16c0ad63a482dbab6c688334e62a8c804
Shadaj Laddad <shadaj@users.noreply.github.com>
2015-01-30 05:20:58 +0000

    Initial static file serving
    git-svn-id: http://svn.lynbrookrobotics.com/code15@64 9f8849d2-cf13-4b18-a094-84ffabbe6057

---
+++ Robot/LRT15/src/dashboard/DashboardLogger.java
@@ -0,0 +1,86 @@
+package dashboard;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelPipeline;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+
+import java.io.File;
+import java.util.Queue;
+
+import com.corundumstudio.socketio.Configuration;
+import com.corundumstudio.socketio.SocketIOChannelInitializer;
+import com.corundumstudio.socketio.SocketIOServer;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.DriverStation;
+
+public class DashboardLogger
+{
+	private static DashboardLogger instance;
+	
+	private Configuration config;
+	private SocketIOServer server;
+//	private AnalogInput prox = new AnalogInput(0);
+	
+	public DashboardLogger() {
+		
+		System.out.println("startin server");
+		config = new Configuration();
+		config.setHostname("localhost");
+		config.setPort(8081);
+		config.getSocketConfig().setReuseAddress(true);
+		server = new SocketIOServer(config);
+		
+		server.setPipelineFactory(new StaticSocketIOChannel(config));
+		server.start();
+	}
+	
+	public void log(DashboardLog message) {
+		server.getBroadcastOperations().sendEvent("data-update", message.json());
+	}
+	
+	public static DashboardLogger getInstance() {
+		if (instance == null) {
+			instance = new DashboardLogger();
+		}
+		
+		return instance;
+	}
+	
+	public void tick() {
+//		DashboardLogger.getInstance().log(new IntegerLog("motor-speed", (int) (prox.getAverageVoltage())));
+		log(new BooleanLog("robot-on", DriverStation.getInstance().isEnabled()));
+	}
+	
+	class StaticSocketIOChannel extends SocketIOChannelInitializer {
+		Configuration configuration;
+		public StaticSocketIOChannel(Configuration configuration)
+		{
+			super();
+			this.configuration = configuration;
+		}
+		@Override
+	    protected void initChannel(Channel ch) throws Exception {
+	        ChannelPipeline pipeline = ch.pipeline();
+	        addSslHandler(pipeline);
+	        addSocketioHandlers(pipeline);
+	        pipeline.addAfter(HTTP_ENCODER, "static", new HttpStaticFileServerHandler());
+	    }
+	}
+	
+	public static void main(String[] args) {
+		while (true) {
+			getInstance().log(new IntegerLog("motor-speed", 2));
+			try
+			{
+				Thread.sleep(50);
+			} catch (InterruptedException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
