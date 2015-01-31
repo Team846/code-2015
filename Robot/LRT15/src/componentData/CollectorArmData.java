545c746b8e25c8c469140c4fc929e53ed8585891
Manoj Vasishta <mhvasishta@gmail.com>
2015-01-31 02:20:47 +0000

    CollectorArmData
    git-svn-id: http://svn.lynbrookrobotics.com/code15@66 9f8849d2-cf13-4b18-a094-84ffabbe6057

---
+++ Robot/LRT15/src/componentData/CollectorArmData.java
@@ -0,0 +1,53 @@
+package componentData;
+
+import componentData.ComponentData;
+
+public class CollectorArmData extends ComponentData
+{
+	public enum Position
+	{
+		STOWED,
+		EXTEND
+	}
+	
+	private Position desiredPosition;
+	private Position currentPosition;
+	
+	public CollectorArmData()
+	{
+		super("CollectorArmData");
+		desiredPosition = Position.STOWED;
+		currentPosition = Position.STOWED;
+	}
+	
+	public static CollectorArmData get()
+	{
+		return (CollectorArmData)ComponentData.GetComponentData("CollectorArmData");
+	}
+	
+	public void setDesiredCollectorState(Position newPosition)
+	{
+		desiredPosition = newPosition;
+	}
+	
+	public Position getDesiredCollectorState()
+	{
+		return desiredPosition;
+	}
+	
+	public Position getCurrentCollectorPosition()
+	{
+		return currentPosition;
+	}
+	
+	//Note: no equivalent for cpp friend constructor, will explore package specific modifiers
+	public void setCurrentCollectorPosition(Position p)
+	{
+		currentPosition = p;
+	}
+	
+	@Override
+	protected void ResetCommands() 
+	{	}
+
+}
