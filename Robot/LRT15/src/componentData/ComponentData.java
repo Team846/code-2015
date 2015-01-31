66a9bbab7e036d38b5f32b96bfd51b081c7e8351
Manoj Vasishta <mhvasishta@gmail.com>
2015-01-31 03:26:46 +0000

    Half completed code. Commented out until fix available.
    git-svn-id: http://svn.lynbrookrobotics.com/code15@71 9f8849d2-cf13-4b18-a094-84ffabbe6057

--- Robot/LRT15/src/componentData/ComponentData.java
+++ Robot/LRT15/src/componentData/ComponentData.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import components.CollectorArmData;
 
 public abstract class ComponentData 
 {
--- Robot/LRT15/src/components/Carriage.java
+++ Robot/LRT15/src/components/Carriage.java
@@ -1,34 +1,34 @@
-package components;
-
-public class Carriage extends Component {
-
-	public Carriage() {
-		super("Carriage", driverStationDigitalIn);
-		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	protected void UpdateEnabled() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	protected void UpdateDisabled() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	protected void OnEnabled() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	protected void OnDisabled() {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+//package components;
+//
+//public class Carriage extends Component {
+//
+//	public Carriage() {
+//		super("Carriage", driverStationDigitalIn);
+//		// TODO Auto-generated constructor stub
+//	}
+//
+//	@Override
+//	protected void UpdateEnabled() {
+//		// TODO Auto-generated method stub
+//
+//	}
+//
+//	@Override
+//	protected void UpdateDisabled() {
+//		// TODO Auto-generated method stub
+//
+//	}
+//
+//	@Override
+//	protected void OnEnabled() {
+//		// TODO Auto-generated method stub
+//
+//	}
+//
+//	@Override
+//	protected void OnDisabled() {
+//		// TODO Auto-generated method stub
+//
+//	}
+//
+//}
