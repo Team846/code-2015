b714a4c0151128dbb1563a85f2fba4213ccba0cb
tonypeng1337 <tonypeng1337@9f8849d2-cf13-4b18-a094-84ffabbe6057>
2015-02-04 07:11:58 +0000

    start work on lua
    git-svn-id: http://svn.lynbrookrobotics.com/code15@100 9f8849d2-cf13-4b18-a094-84ffabbe6057

---
+++ Robot/LRT15/src/com/lynbrookrobotics/frc2015/automation/LuaScript.java
@@ -0,0 +1,33 @@
+package com.lynbrookrobotics.frc2015.automation;
+
+public class LuaScript extends Automation {
+
+	public LuaScript() {
+		super("LuaScript", true, true, false);
+	}
+
+	@Override
+	public void AllocateResources() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected boolean Start() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected boolean Abort() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected boolean Run() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
