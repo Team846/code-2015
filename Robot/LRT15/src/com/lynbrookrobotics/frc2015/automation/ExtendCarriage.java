9ae0f3f277833a4667671d08d942cd6c027d4d11
Manoj Vasishta <mhvasishta@gmail.com>
2015-02-07 22:29:13 +0000

    Automation Routine Stubs
    git-svn-id: http://svn.lynbrookrobotics.com/code15@112 9f8849d2-cf13-4b18-a094-84ffabbe6057

---
+++ Robot/LRT15/src/com/lynbrookrobotics/frc2015/automation/ExtendCarriage.java
@@ -0,0 +1,34 @@
+package com.lynbrookrobotics.frc2015.automation;
+
+public class ExtendCarriage extends Automation {
+
+	public ExtendCarriage(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void AllocateResources() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected boolean Start() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected boolean Abort() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected boolean Run() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
