7951d38ef3e5e719b4dfc698da4da20a76b695c6
Shadaj Laddad <shadaj@users.noreply.github.com>
2015-02-16 04:27:41 +0000

    Finish implementation routine loading
    git-svn-id: http://svn.lynbrookrobotics.com/code15@139 9f8849d2-cf13-4b18-a094-84ffabbe6057

--- Robot/LRT15/src/com/team846/frc2015/automation/Autonomous.java
+++ Robot/LRT15/src/com/team846/frc2015/automation/Autonomous.java
@@ -3,6 +3,7 @@ package com.team846.frc2015.automation;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 import org.apache.commons.lang.NotImplementedException;
@@ -27,7 +28,7 @@ public class Autonomous extends Sequential {
 		
 		ClearSequence();
 		
-		LoadRoutine(FindRoutine(1)); //TODO: Configurable routine
+		loadRoutine(FindRoutine(1)); //TODO: Configurable routine
 		
 		ConfigRuntime.ConfigureAll();
 		
@@ -56,97 +57,191 @@ public class Autonomous extends Sequential {
 		
 		while (in.hasNext()) {
 			String line = in.nextLine();
-			String[] parallelSplit = line.split("||");
-			Automation[] parallelRoutines = new Automation[parallelSplit.length];
+			String[] parallelSplit = line.split("\\|\\|");
+			ArrayList<Automation> parallelRoutines = new ArrayList<Automation>(parallelSplit.length);
+			System.out.println("begin one parallel group:");
 			for (int i = 0; i < parallelSplit.length; i++) {
 				String automation = parallelSplit[i].trim();
-				String[] commandSplit = automation.split("(");
+				String[] commandSplit = automation.split("\\(");
+				String command = commandSplit[0];
 				String args = commandSplit[1].substring(0, commandSplit[1].length() - 1);
 				String[] argsSplit = args.split(",");
+				for (int argsI = 0; argsI < argsSplit.length; argsI++) {
+					argsSplit[argsI] = argsSplit[argsI].trim();
+				}
+				
+				System.out.println(command + ": " + Arrays.toString(argsSplit));
+				
+				try {
+					switch(command) {
+						case "collect":
+							switch(argsSplit.length) {
+								case 0:
+									parallelRoutines.set(i, new Collect());
+									break;
+								case 1:
+									parallelRoutines.set(i, new Collect(parseBoolean(argsSplit[0])));
+									break;
+								case 2:
+									parallelRoutines.set(i, new Collect(parseBoolean(argsSplit[0]), parseBoolean(argsSplit[1])));
+									break;
+							}
+							break;
+							
+						case "elevate":
+							switch(argsSplit.length) {
+								case 1:
+									parallelRoutines.set(i, new Elevate(Integer.parseInt(argsSplit[0])));
+									break;
+							}
+							break;
+							
+						case "extendCarriage":
+							switch(argsSplit.length) {
+								case 0:
+									parallelRoutines.set(i, new ExtendCarriage());
+									break;
+								case 1:
+									parallelRoutines.set(i, new ExtendCarriage(Double.parseDouble(argsSplit[0])));
+									break;
+							}
+							break;
+							
+						case "loadContainer":
+							switch(argsSplit.length) {
+								case 0:
+									parallelRoutines.set(i, new LoadContainer());
+									break;
+							}
+							break;
+							
+						case "loadTote":
+							switch(argsSplit.length) {
+								case 0:
+									parallelRoutines.set(i, new LoadTote());
+									break;
+							}
+							break;
+							
+						case "loadUprightContainer":
+							switch(argsSplit.length) {
+								case 0:
+									parallelRoutines.set(i, new LoadUprightContainer());
+									break;
+							}
+							break;
+							
+						case "releaseStack":
+							switch(argsSplit.length) {
+								case 0:
+									parallelRoutines.set(i, new ReleaseStack());
+									break;
+							}
+							break;
+					}
+				} catch(Exception e) {
+					AsyncPrinter.error(e.getMessage());
+				}
+			}
+			
+			if (parallelRoutines.size() == 1) {
+				AddAutomation(parallelRoutines.get(0));
+			} else {
+				AddAutomation(new Parallel("routineParallel", parallelRoutines));
 			}
 		}
 	}
 	
-	private void LoadRoutine(String path)
-	{
-		Scanner in = null;
-		try {
-			 in = new Scanner(new File(path));
-		} catch (FileNotFoundException e) {
-			AsyncPrinter.error("Cannot open autonomous file path: " + path);
-			e.printStackTrace();
+	private boolean parseBoolean(String string) throws Exception {
+		if (string == "true") {
+			return true;
+		} else if (string == "false") {
+			return false;
+		} else {
+			throw new Exception("unable to parse boolean: " + string);
 		}
-		int lineNumber = 1;
-		while(in.hasNext())
-		{
-			String line = in.nextLine();
-			line = line.substring(0, line.indexOf('#'));
-			line = line.replaceAll("\\s+", "");
-			if(line.length() == 0)
-				continue;
-			
-			ArrayList<String> routineList = new ArrayList<String>();
-			int pos;
-			
-			while(line.contains("),"))
-			{
-				pos = line.indexOf("),");
-				routineList.add(line.substring(0, pos + 1));
-				line = line.substring(pos + 2); //TODO: uncertain
-			}
-			routineList.add(line);
-			ArrayList<Automation> parallelRoutines = new ArrayList<Automation>();
-			for(String s : routineList)
-			{
-				Automation current;
-				String command, args;
-				ArrayList<String> argList = new ArrayList<String>();
-				String temp;
-				command = s.substring(0, s.indexOf('('));
-				args = s.substring( s.indexOf('(') + 1, s.lastIndexOf(')'));
-				while(args.contains(","))
-				{
-					argList.add(args.substring(0, args.indexOf(',')));
-					args = args.substring(args.indexOf(',')+1);
-				}
-				boolean failed = false;
-				
-				//COMMAND CASES
-				if(true) //TODO:add automation cases
-					;
-				else
-				{
-					AsyncPrinter.warn("Unknwon routine " + command + " on line " + lineNumber + " ignoring");
-					continue;
-				}
-				if(failed)
-				{
-					AsyncPrinter.warn("Incorrect number of args for " + command + " on line " + lineNumber + " ignoring");					
-					continue;
-				}
-				if(parallelRoutines.size() > 1)
-				{
-					AddAutomation(new Parallel("AutonomousParllel", parallelRoutines));
-				}
-				else if(parallelRoutines.size() == 1)
-				{
-					AddAutomation(parallelRoutines.get(0));
-				}
-				lineNumber++;
-				
-			}
-			AsyncPrinter.info("Done loading auto routine at " + path);
-			in.close();
-			
-			
-		}
-		
 	}
+	
+//	private void LoadRoutine(String path)
+//	{
+//		Scanner in = null;
+//		try {
+//			 in = new Scanner(new File(path));
+//		} catch (FileNotFoundException e) {
+//			AsyncPrinter.error("Cannot open autonomous file path: " + path);
+//			e.printStackTrace();
+//		}
+//		int lineNumber = 1;
+//		while(in.hasNext())
+//		{
+//			String line = in.nextLine();
+//			line = line.substring(0, line.indexOf('#'));
+//			line = line.replaceAll("\\s+", "");
+//			if(line.length() == 0)
+//				continue;
+//			
+//			ArrayList<String> routineList = new ArrayList<String>();
+//			int pos;
+//			
+//			while(line.contains("),"))
+//			{
+//				pos = line.indexOf("),");
+//				routineList.add(line.substring(0, pos + 1));
+//				line = line.substring(pos + 2); //TODO: uncertain
+//			}
+//			routineList.add(line);
+//			ArrayList<Automation> parallelRoutines = new ArrayList<Automation>();
+//			for(String s : routineList)
+//			{
+//				Automation current;
+//				String command, args;
+//				ArrayList<String> argList = new ArrayList<String>();
+//				String temp;
+//				command = s.substring(0, s.indexOf('('));
+//				args = s.substring( s.indexOf('(') + 1, s.lastIndexOf(')'));
+//				while(args.contains(","))
+//				{
+//					argList.add(args.substring(0, args.indexOf(',')));
+//					args = args.substring(args.indexOf(',')+1);
+//				}
+//				boolean failed = false;
+//				
+//				//COMMAND CASES
+//				if(true) //TODO:add automation cases
+//					;
+//				else
+//				{
+//					AsyncPrinter.warn("Unknwon routine " + command + " on line " + lineNumber + " ignoring");
+//					continue;
+//				}
+//				if(failed)
+//				{
+//					AsyncPrinter.warn("Incorrect number of args for " + command + " on line " + lineNumber + " ignoring");					
+//					continue;
+//				}
+//				if(parallelRoutines.size() > 1)
+//				{
+//					AddAutomation(new Parallel("AutonomousParllel", parallelRoutines));
+//				}
+//				else if(parallelRoutines.size() == 1)
+//				{
+//					AddAutomation(parallelRoutines.get(0));
+//				}
+//				lineNumber++;
+//				
+//			}
+//			AsyncPrinter.info("Done loading auto routine at " + path);
+//			in.close();
+//			
+//			
+//		}
+//		
+//	}
 	
 	private String FindRoutine(int routineNumber)
 	{
 		throw new NotImplementedException("Customizable routine not ");
 	}
 	
-
+                                                                                         
 }
