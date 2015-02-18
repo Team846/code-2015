346c48990e923297274fd33bdd9efe2290c630bf
wafflemans <tyang108@student.fuhsd.org>
2015-02-18 00:51:35 +0000

    nuked watchdog
    git-svn-id: http://svn.lynbrookrobotics.com/code15@149 9f8849d2-cf13-4b18-a094-84ffabbe6057

--- Robot/LRT15/src/com/team846/frc2015/utils/Watchdog.java
+++ Robot/LRT15/src/com/team846/frc2015/utils/Watchdog.java
@@ -1,39 +1,39 @@
-package com.team846.frc2015.utils;
-import java.util.Timer;
-import java.util.TimerTask;
-
-public class Watchdog extends Timer{
-
-	static Timer timer;
-	
-	static int time;
-	static long killTime = time;
-	
-	static TimerTask kill = Watchdog.kill();
-	
-	public Watchdog(int time){
-		super();
-		Watchdog.time = time;
-		
-	}
-	
-	public static void start(){
-		timer = new Timer();
-		timer.schedule(kill, killTime);
-		
-	}
-	
-	public static void feed(){
-		//feed
-		System.out.println("Fed the dog");
-		timer.cancel();
-		Watchdog.start();
-		
-	}
-	
-	public static TimerTask kill(){		
-		//kill
-		System.out.print("Killed watchdog.");
-		return kill;
-	}
-}
+//package com.team846.frc2015.utils;
+//import java.util.Timer;
+//import java.util.TimerTask;
+//
+//public class Watchdog extends Timer{
+//
+//	static Timer timer;
+//	
+//	static int time;
+//	static long killTime = time;
+//	
+//	static TimerTask kill = Watchdog.kill();
+//	
+//	public Watchdog(int time){
+//		super();
+//		Watchdog.time = time;
+//		
+//	}
+//	
+//	public static void start(){
+//		timer = new Timer();
+//		timer.schedule(kill, killTime);
+//		
+//	}
+//	
+//	public static void feed(){
+//		//feed
+//		System.out.println("Fed the dog");
+//		timer.cancel();
+//		Watchdog.start();
+//		
+//	}
+//	
+//	public static TimerTask kill(){		
+//		//kill
+//		System.out.print("Killed watchdog.");
+//		return kill;
+//	}
+//}
