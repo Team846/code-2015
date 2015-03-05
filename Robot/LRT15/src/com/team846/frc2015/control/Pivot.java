e340ec03160c313b4012907309bb7f4d72449396
dtjong <davintjong@gmail.com>
2015-03-05 03:56:38 +0000

    Updated pivot class -able to calculate angular velocity -able to calculate tangential velocity
    git-svn-id: http://svn.lynbrookrobotics.com/code15@181 9f8849d2-cf13-4b18-a094-84ffabbe6057

--- Robot/LRT15/src/com/team846/frc2015/control/Pivot.java
+++ Robot/LRT15/src/com/team846/frc2015/control/Pivot.java
@@ -4,43 +4,35 @@ import com.team846.frc2015.utils.Pair;
 
 public class Pivot
 {
-	Pair<Double, Double> point;
-	double dpt;
-	double magnitude;
-
-	public double update()
+	double R;
+	
+	public Pivot(double r){
+		this.R = r;
+	}
+	
+	public void update(double angularChange)
 	{
-		magnitude = findRadii(point);
-		double angle = dpt / magnitude;
-		return angle;
+		double W = angularVelocity(angularChange);
+		double strafe = strafeVelocity(W);
 	}
 
 	private double findRadii(Pair<Double, Double> pnt)
 	{
 		double mag = 0;
-		mag = Math.sqrt(Math.pow(pnt.getFirst(), 2)
-				+ Math.pow(pnt.getSecond(), 2));
+		mag = Math.sqrt(Math.pow(pnt.getFirst(), 2) + Math.pow(pnt.getSecond(), 2));
 		return mag;
 	}
-
-	public double getDpt()
-	{
-		return dpt;
+	
+	private double angularVelocity(double angleChange){
+		double w = 0;
+		w = angleChange/0.02;
+		
+		return w;
 	}
-
-	public void setDpt(double dpt)
-	{
-		this.dpt = dpt;
+	
+	private double strafeVelocity(double W){
+		double Vt = W * R;
+		
+		return Vt;
 	}
-
-	public Pair<Double, Double> getPoint()
-	{
-		return point;
-	}
-
-	public void setPoint(Pair<Double, Double> point)
-	{
-		this.point = point;
-	}
-
 }
