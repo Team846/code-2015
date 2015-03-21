742628992905365854ba36f96426bb619c62b15f
Shadaj Laddad <shadaj@users.noreply.github.com>
2015-03-21 22:18:09 +0000

    Fix more string checks
    git-svn-id: http://svn.lynbrookrobotics.com/code15@205 9f8849d2-cf13-4b18-a094-84ffabbe6057

--- Robot/LRT15/src/com/team846/frc2015/automation/Autonomous.java
+++ Robot/LRT15/src/com/team846/frc2015/automation/Autonomous.java
@@ -3,10 +3,7 @@ package com.team846.frc2015.automation;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
-
-import org.apache.commons.lang.NotImplementedException;
 
 import com.team846.frc2015.config.ConfigRuntime;
 import com.team846.frc2015.config.RobotConfig;
@@ -81,218 +78,202 @@ public class Autonomous extends Sequential
 			ArrayList<Automation> parallelRoutines = new ArrayList<Automation>(
 					parallelSplit.length);
 
-			for (int i = 0; i < parallelSplit.length; i++)
-			{
-				String automation = parallelSplit[i].trim();
+            for (String aParallelSplit : parallelSplit) {
+                String automation = aParallelSplit.trim();
 
-				String[] commandSplit = automation.split("\\("); // splits at "(" separating command name + args
+                String[] commandSplit = automation.split("\\("); // splits at "(" separating command name + args
 
-				String command = commandSplit[0];
+                String command = commandSplit[0];
 
-				String args = commandSplit[1].substring(0,
-						commandSplit[1].length() - 1); //getting rid of last paren
-				String[] argsSplit = args.split(",");
+                String args = commandSplit[1].substring(0,
+                        commandSplit[1].length() - 1); //getting rid of last paren
+                String[] argsSplit = args.split(",");
 
-				for (int argsI = 0; argsI < argsSplit.length; argsI++)
-					argsSplit[argsI] = argsSplit[argsI].trim();
+                for (int argsI = 0; argsI < argsSplit.length; argsI++)
+                    argsSplit[argsI] = argsSplit[argsI].trim();
 
 //				try
 //				{
-					switch (command)
-					{
-					case "test":
-						switch (argsSplit.length)
-						{
-						case 0:
-							parallelRoutines.add( new Elevate(Integer.parseInt(argsSplit[0])));
-							break;
-						}
-						break;
+                switch (command) {
+                    case "test":
+                        switch (argsSplit.length) {
+                            case 0:
+                                parallelRoutines.add(new Elevate(Integer.parseInt(argsSplit[0])));
+                                break;
+                        }
+                        break;
 
-					case "elevate":
-						switch (argsSplit.length)
-						{
-						case 1:
-							parallelRoutines.add( new Elevate(Integer.parseInt(argsSplit[0])));
-							break;
-						}
-						break;
+                    case "elevate":
+                        switch (argsSplit.length) {
+                            case 1:
+                                parallelRoutines.add(new Elevate(Integer.parseInt(argsSplit[0])));
+                                break;
+                        }
+                        break;
 
-					case "extendCarriage":
-						switch (argsSplit.length)
-						{
-						case 0:
-							parallelRoutines.add( new ExtendCarriage());
-							break;
-						case 1:
-							parallelRoutines.add( new ExtendCarriage(Double.parseDouble(argsSplit[0])));
-							break;
-						}
-						break;
+                    case "extendCarriage":
+                        switch (argsSplit.length) {
+                            case 0:
+                                parallelRoutines.add(new ExtendCarriage());
+                                break;
+                            case 1:
+                                parallelRoutines.add(new ExtendCarriage(Double.parseDouble(argsSplit[0])));
+                                break;
+                        }
+                        break;
 
-					case "loadTote":
-						switch (argsSplit.length)
-						{
-						case 0:
-							parallelRoutines.add( new LoadTote(true));
-							break;
-						}
-						break;
+                    case "loadTote":
+                        switch (argsSplit.length) {
+                            case 0:
+                                parallelRoutines.add(new LoadTote(true));
+                                break;
+                        }
+                        break;
 
-					case "loadUprightContainer":
-						switch (argsSplit.length)
-						{
-						case 0:
-							parallelRoutines.add( new LoadUprightContainer(true));
-							break;
-						}
-						break;
-						
-					case "loadSidewaysContainer":
-						switch(argsSplit.length)
-						{
-						case 0:
-							parallelRoutines.add( new LoadSidewaysContainer(true));
-							break;
-						}
+                    case "loadUprightContainer":
+                        switch (argsSplit.length) {
+                            case 0:
+                                parallelRoutines.add(new LoadUprightContainer(true));
+                                break;
+                        }
+                        break;
 
-					case "releaseStack":
-						switch (argsSplit.length)
-						{
-						case 0:
-							parallelRoutines.add( new ReleaseStack());
-							break;
-						}
-						break;
-					case "drive":
-						switch (argsSplit.length)
-						{
-						case 1:
-							parallelRoutines.add( new Drive(Double.parseDouble(argsSplit[0])));
-							break;
-						case 2:
-							parallelRoutines.add( new Drive(Double.parseDouble(argsSplit[0])
-									,Double.parseDouble(argsSplit[1])));
-							break;
-						case 3:
-							parallelRoutines.add( new Drive(Double.parseDouble(argsSplit[0]),
-									Double.parseDouble(argsSplit[1]),
-									Double.parseDouble(argsSplit[2])));
-							break;
-						case 4:
-							parallelRoutines.add( new Drive(Double.parseDouble(argsSplit[0]),
-									Double.parseDouble(argsSplit[1]),
-									Double.parseDouble(argsSplit[2]),
-									Boolean.parseBoolean(argsSplit[3])));
-							break;
-						}
-						break;
-					case "turn":
-						switch (argsSplit.length)
-						{
-						case 0:
-							parallelRoutines.add( new Turn());
-							break;
-						case 3:
-							parallelRoutines.add( new Turn(Double.parseDouble(argsSplit[0]),
-									Double.parseDouble(argsSplit[1]),
-									Double.parseDouble(argsSplit[2])));
-							break;
-						}
-						break;
-					case "pause":
-						switch (argsSplit.length)
-						{
-						case 1:
-							parallelRoutines.add( new Pause(Double.parseDouble(argsSplit[0])));
-							break;
-						}
-						break;
-					case "strafe":
-						switch (argsSplit.length)
-						{
-						case 1:
-							parallelRoutines.add( new Strafe(Double.parseDouble(argsSplit[0])));
-							break;
-						case 2:
-							parallelRoutines.add( new Strafe(Double.parseDouble(argsSplit[0]),
-									Double.parseDouble(argsSplit[1])));
-							break;
-						}
-						break;
-					case "load+elevate":
-						switch (argsSplit.length)
-						{
-						case 1:
-							Sequential routine = new Sequential("LoadElevate");
-							routine.AddAutomation(new LoadTote(true));
-							routine.AddAutomation(new Elevate(Integer.parseInt(argsSplit[0])));
-							parallelRoutines.add(routine);
-						}
-						break;
-					case "loadAdditional+elevate":
-						switch (argsSplit.length)
-						{
-						case 1:
-							Sequential routine = new Sequential("LoadAdditionalElevate");
-							routine.AddAutomation(new LoadAdditional(true));
-							routine.AddAutomation(new Elevate(Integer.parseInt(argsSplit[0])));
-							parallelRoutines.add(routine);
-						}
-						break;
-					case "drive+sweep":
-						Parallel routine = new Parallel("DriveSweep", true);
-						switch (argsSplit.length)
-						{
-						case 2:
-							routine.AddAutomation( new Drive(Double.parseDouble(argsSplit[0])));
-							if (argsSplit[1] == "left")
-								routine.AddAutomation(new Sweep(Sweep.Direction.LEFT));
-							else if (argsSplit[1] == "right")
-								routine.AddAutomation(new Sweep(Sweep.Direction.RIGHT));
-							break;
-						case 3:
-							routine.AddAutomation( new Drive(Double.parseDouble(argsSplit[0])
-									,Double.parseDouble(argsSplit[1])));
-							if (argsSplit[2] == "left")
-								routine.AddAutomation(new Sweep(Sweep.Direction.LEFT));
-							else if (argsSplit[2] == "right")
-								routine.AddAutomation(new Sweep(Sweep.Direction.RIGHT));
-							break;
-						case 4:
-							routine.AddAutomation( new Drive(Double.parseDouble(argsSplit[0]),
-									Double.parseDouble(argsSplit[1]),
-									Double.parseDouble(argsSplit[2])));
-							if (argsSplit[3] == "left")
-								routine.AddAutomation(new Sweep(Sweep.Direction.LEFT));
-							else if (argsSplit[3] == "right")
-								routine.AddAutomation(new Sweep(Sweep.Direction.RIGHT));
-							break;
-						case 5:
-							routine.AddAutomation( new Drive(Double.parseDouble(argsSplit[0]),
-									Double.parseDouble(argsSplit[1]),
-									Double.parseDouble(argsSplit[2]),
-									Boolean.parseBoolean(argsSplit[3])));
-							AsyncPrinter.error("sweep+arg: " + argsSplit[4]);
+                    case "loadSidewaysContainer":
+                        switch (argsSplit.length) {
+                            case 0:
+                                parallelRoutines.add(new LoadSidewaysContainer(true));
+                                break;
+                        }
+
+                    case "releaseStack":
+                        switch (argsSplit.length) {
+                            case 0:
+                                parallelRoutines.add(new ReleaseStack());
+                                break;
+                        }
+                        break;
+                    case "drive":
+                        switch (argsSplit.length) {
+                            case 1:
+                                parallelRoutines.add(new Drive(Double.parseDouble(argsSplit[0])));
+                                break;
+                            case 2:
+                                parallelRoutines.add(new Drive(Double.parseDouble(argsSplit[0])
+                                        , Double.parseDouble(argsSplit[1])));
+                                break;
+                            case 3:
+                                parallelRoutines.add(new Drive(Double.parseDouble(argsSplit[0]),
+                                        Double.parseDouble(argsSplit[1]),
+                                        Double.parseDouble(argsSplit[2])));
+                                break;
+                            case 4:
+                                parallelRoutines.add(new Drive(Double.parseDouble(argsSplit[0]),
+                                        Double.parseDouble(argsSplit[1]),
+                                        Double.parseDouble(argsSplit[2]),
+                                        Boolean.parseBoolean(argsSplit[3])));
+                                break;
+                        }
+                        break;
+                    case "turn":
+                        switch (argsSplit.length) {
+                            case 0:
+                                parallelRoutines.add(new Turn());
+                                break;
+                            case 3:
+                                parallelRoutines.add(new Turn(Double.parseDouble(argsSplit[0]),
+                                        Double.parseDouble(argsSplit[1]),
+                                        Double.parseDouble(argsSplit[2])));
+                                break;
+                        }
+                        break;
+                    case "pause":
+                        switch (argsSplit.length) {
+                            case 1:
+                                parallelRoutines.add(new Pause(Double.parseDouble(argsSplit[0])));
+                                break;
+                        }
+                        break;
+                    case "strafe":
+                        switch (argsSplit.length) {
+                            case 1:
+                                parallelRoutines.add(new Strafe(Double.parseDouble(argsSplit[0])));
+                                break;
+                            case 2:
+                                parallelRoutines.add(new Strafe(Double.parseDouble(argsSplit[0]),
+                                        Double.parseDouble(argsSplit[1])));
+                                break;
+                        }
+                        break;
+                    case "load+elevate":
+                        switch (argsSplit.length) {
+                            case 1:
+                                Sequential routine = new Sequential("LoadElevate");
+                                routine.AddAutomation(new LoadTote(true));
+                                routine.AddAutomation(new Elevate(Integer.parseInt(argsSplit[0])));
+                                parallelRoutines.add(routine);
+                        }
+                        break;
+                    case "loadAdditional+elevate":
+                        switch (argsSplit.length) {
+                            case 1:
+                                Sequential routine = new Sequential("LoadAdditionalElevate");
+                                routine.AddAutomation(new LoadAdditional(true));
+                                routine.AddAutomation(new Elevate(Integer.parseInt(argsSplit[0])));
+                                parallelRoutines.add(routine);
+                        }
+                        break;
+                    case "drive+sweep":
+                        Parallel routine = new Parallel("DriveSweep", true);
+                        switch (argsSplit.length) {
+                            case 2:
+                                routine.AddAutomation(new Drive(Double.parseDouble(argsSplit[0])));
+                                if (argsSplit[1].equals("left"))
+                                    routine.AddAutomation(new Sweep(Sweep.Direction.LEFT));
+                                else if (argsSplit[1].equals("right"))
+                                    routine.AddAutomation(new Sweep(Sweep.Direction.RIGHT));
+                                break;
+                            case 3:
+                                routine.AddAutomation(new Drive(Double.parseDouble(argsSplit[0])
+                                        , Double.parseDouble(argsSplit[1])));
+                                if (argsSplit[2].equals("left"))
+                                    routine.AddAutomation(new Sweep(Sweep.Direction.LEFT));
+                                else if (argsSplit[2].equals("right"))
+                                    routine.AddAutomation(new Sweep(Sweep.Direction.RIGHT));
+                                break;
+                            case 4:
+                                routine.AddAutomation(new Drive(Double.parseDouble(argsSplit[0]),
+                                        Double.parseDouble(argsSplit[1]),
+                                        Double.parseDouble(argsSplit[2])));
+                                if (argsSplit[3].equals("left"))
+                                    routine.AddAutomation(new Sweep(Sweep.Direction.LEFT));
+                                else if (argsSplit[3].equals("right"))
+                                    routine.AddAutomation(new Sweep(Sweep.Direction.RIGHT));
+                                break;
+                            case 5:
+                                routine.AddAutomation(new Drive(Double.parseDouble(argsSplit[0]),
+                                        Double.parseDouble(argsSplit[1]),
+                                        Double.parseDouble(argsSplit[2]),
+                                        Boolean.parseBoolean(argsSplit[3])));
+                                AsyncPrinter.error("sweep+arg: " + argsSplit[4]);
 //							if (argsSplit[4] == "left")
 //							{
 //
 //								AsyncPrinter.error("creatinglef+arg: " + argsSplit[4]);
-								routine.AddAutomation(new Sweep(Sweep.Direction.LEFT));
+                                routine.AddAutomation(new Sweep(Sweep.Direction.LEFT));
 //							}
 //							else if (argsSplit[4] == "right")
 //								routine.AddAutomation(new Sweep(Sweep.Direction.RIGHT));
-							break;
-						}
-						parallelRoutines.add(routine);
-						break;
-					}
-					
+                                break;
+                        }
+                        parallelRoutines.add(routine);
+                        break;
+                }
+
 //				} catch (Exception e)
 //				{
 //					AsyncPrinter.error( e.toString());
 //				}
-			}
+            }
 			
 			if (parallelRoutines.size() == 0)
 			{
