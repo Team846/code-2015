6b54645f57632997d25564167f125a8f974c770f
Shadaj Laddad <shadaj@users.noreply.github.com>
2015-09-06 01:39:29 +0000

    Implement file watching in new config implementation
    git-svn-id: http://svn.lynbrookrobotics.com/code15@309 9f8849d2-cf13-4b18-a094-84ffabbe6057

--- Robot/LRT15/src/com/team846/frc2015/config/RootConfig.java
+++ Robot/LRT15/src/com/team846/frc2015/config/RootConfig.java
@@ -1,17 +1,37 @@
 package com.team846.frc2015.config;
 
+import static com.team846.frc2015.logging.AsyncLogger.*;
 import com.team846.util.monads.CachedMonad;
 import com.team846.util.monads.MutableCachedMonadSource;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchKey;
 
 public class RootConfig {
     private MutableCachedMonadSource<Config> mutableRoot = new MutableCachedMonadSource<>();
 
     public RootConfig(File configFile) {
         mutableRoot.set(ConfigFactory.parseFile(configFile));
+
+        try {
+            WatchKey key = configFile.toPath().register(FileSystems.getDefault().newWatchService(), StandardWatchEventKinds.ENTRY_MODIFY);
+            Thread watcher = new Thread() {
+                public void run() {
+                    key.pollEvents().stream().filter(event -> event.kind() != StandardWatchEventKinds.OVERFLOW).forEach(event ->
+                        mutableRoot.set(ConfigFactory.parseFile(configFile))
+                    );
+                }
+            };
+
+            watcher.start();
+        } catch (IOException e) {
+            error("[RootConfig] Unable to register watcher on config file");
+        }
     }
 
     public CachedMonad<Config> get() {
